a69e70c482e12819d20dd8fb1b8cd9e5770834d7
dineshthepro <dinesh@theproindia.com>
2021-11-15 17:44:57 +0530

    headless

--- src/test/java/com/stepdefinitions/GPMDstepdef.java
+++ src/test/java/com/stepdefinitions/GPMDstepdef.java
@@ -29,7 +29,7 @@ public class GPMDstepdef extends MainpageforGPMDtest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        // options.addArguments("--headless");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
